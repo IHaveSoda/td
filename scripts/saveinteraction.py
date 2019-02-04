697ead744297e227daed69e88ab2b67061ce8f2e
IHaveSoda <46517410+IHaveSoda@users.noreply.github.com>
2019-02-04 20:15:02 +0100

    Added safety to saveinteraction
    -Made sure that __cached/game.data would be created of it doesnt exist

--- scripts/saveinteraction.py
+++ scripts/saveinteraction.py
@@ -1,15 +1,27 @@
 import pickle
+import os
 
 standardsave = "__cached/game.data"
+
+def check_save_exists():
+	if os.path.exists("__cached/game.data"):
+		pass
+	else:
+		with open("__cached/game.data", "w") as f:
+			f.write("")
+
+
 
 def formatsave(self):
 	return [self.gamedata, self.towers]
 
 def save_data(self):
+	check_save_exists()
 	with open(standardsave, "wb") as f:
 		pickle.dump(formatsave(self), f)
 
 def load_data(self):
+	check_save_exists()
 	with open(standardsave, "rb") as f:
 		read_data = pickle.load(f)
 
@@ -17,5 +29,6 @@ def load_data(self):
 	self.towers = read_data[1]
 
 def clear():
+	check_save_exists()
 	with open(standardsave, "wb") as f:
 		pickle.dump([], f)
